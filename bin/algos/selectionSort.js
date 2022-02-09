4faa82569502871cb0d557a2ed17b8adaae542aa
Adrien35240 <adriendev35240@gmail.com>
2022-02-09 23:38:43 +0100

    feat:add selection sort algo

--- bin/algos/selectionSort.js
+++ bin/algos/selectionSort.js
@@ -1,19 +1,21 @@
 
-export function selectionSort(ctx, data) {
-    let n = data.length;
+export function selectionSort(chart) {
+    let n = chart.data.length;
 
     for (let i = 0; i < n; i++) {
         let min = i;
+        setTimeout(function () {
         for (let j = i + 1; j < n; j++) {
-            if (data[j] < data[min]) {
+            if (chart.data[j] < chart.data[min]) {
                 min = j;
             }
         }
         if (min != i) {
-            let tmp = data[i];
-            data[i] = data[min];
-            data[min] = tmp;
+            let tmp = chart.data[i];
+            chart.data[i] = chart.data[min];
+            chart.data[min] = tmp;
+            chart.update()
         }
+        }, 10);
     }
-    return data;
 }
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import "./style.css";
 import { graph } from "./bin/class/graph";
 import { bubbleSort } from "./bin/algos/bubbleSort";
+import { selectionSort } from "./bin/algos/selectionSort";
 import { generateData, generateLabel, shuffle } from "./bin/functions/data";
 const app = {
   selectMax: null,
@@ -15,7 +16,8 @@ const app = {
   runBtn: null,
   shuffleBtn:null,
   arrayChart: [],
-  addBtn:null,
+  addBtn: null,
+  algo: "Bubble sort",
   init() {
     app.canvasContainer = document.getElementById("canvas__container");
     //select nbr data
@@ -31,7 +33,15 @@ const app = {
     app.runBtn = document.getElementById('run__btn')
     app.runBtn.addEventListener('click', () => {
       app.arrayChart.forEach(chart => {
-      bubbleSort(chart)
+        switch (chart.algo) {
+          case 'Bubble sort':
+            bubbleSort(chart)
+            break;
+          case 'Selection sort':
+            selectionSort(chart)
+          default:
+            console.log(`Sorry,no algo found.`);
+        }
       })
     })
     //btn shuffle data
@@ -42,15 +52,16 @@ const app = {
         chart.shuffle(app.data,app.label)
       })
     })
+   
     // add btn
     app.addBtn = document.getElementById('add__btn')
     app.addBtn.addEventListener('click', () => {
       app.canvasId++
       app.createTemplate(app.canvasId)
-      app.arrayChart.forEach(chart => {
-        app.randomizeData(app.max)
-        chart.shuffle(app.data, app.label)
-      })
+      // app.arrayChart.forEach(chart => {
+      //   app.randomizeData(app.max)
+      //   chart.shuffle(app.data, app.label)
+      // })
     })
   },
   randomizeData(max) {
@@ -67,11 +78,23 @@ const app = {
     const canvas = clone.querySelectorAll('canvas')
     canvas[0].setAttribute("id", `canvas${id}`)
     const description = clone.getElementById('description')
-    description.innerHTML = `Bubble Sort est l'algorithme de tri simple </br> qui fonctionne en échangeant les éléments adjacents</br> s'ils sont dans le mauvais ordre.`
+    description.innerHTML = `Bubble Sort est un algorithme de tri simple </br> qui fonctionne en échangeant les éléments adjacents</br> s'ils sont dans le mauvais ordre.`
+    //select algo
+    app.algoBtn = clone.getElementById('algo__btn')
+    app.algoBtn.addEventListener('change', () => {
+      if (this.algoBtn.value === "Bubble sort") {
+        app.chart.algo = "Bubble sort"
+        description.innerHTML = `Bubble Sort est un algorithme de tri simple </br> qui fonctionne en échangeant les éléments adjacents</br> s'ils sont dans le mauvais ordre.`
+      }
+      if (this.algoBtn.value === "Selection sort") {
+        app.chart.algo = "Selection sort"
+        description.innerHTML = `L'algorithme de tri par sélection trie un tableau en trouvant à plusieurs </br> reprises l'élément minimum (en tenant compte de l'ordre croissant)</br>  à partir de la partie non triée et en le plaçant au début.</br> L'algorithme maintient deux sous-tableaux dans un tableau donné.`
+      }
+    })
     app.canvasContainer.appendChild(clone)
     // -------template--------
     app.ctx = document.getElementById(`canvas${id}`).getContext('2d')
-    app.chart = new graph(app.canvasId, app.ctx, app.data, app.label)
+    app.chart = new graph(app.canvasId, app.ctx, app.data, app.label,app.algo)
     // tableau de graphs
     app.arrayChart.push(app.chart)
     app.chart.init()
