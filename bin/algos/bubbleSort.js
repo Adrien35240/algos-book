212c7475945897e426ff082f016fc19507452609
Adrien35240 <adriendev35240@gmail.com>
2022-02-09 23:54:20 +0100

    fix:speed of selection sort

--- bin/algos/bubbleSort.js
+++ bin/algos/bubbleSort.js
@@ -1,14 +1,15 @@
 export function bubbleSort(chart) {
     for (let i = 0; i < chart.data.length; i++) {
-        for (let j = 0; j < chart.data.length; j++) {
-           setTimeout(function () {
+        setTimeout(function () {
+        for (let j = 0; j < chart.data.length; j++) { 
                 if (chart.data[j] > chart.data[j + 1]) {
                     let temp = chart.data[j];
                     chart.data[j] = chart.data[j + 1];
                     chart.data[j + 1] = temp;
-                     chart.update();
+                     
                 }
-            }, 1);
-        }
+            }
+            chart.update();
+        }, 100);
     }
 }
--- bin/algos/selectionSort.js
+++ bin/algos/selectionSort.js
@@ -4,8 +4,8 @@ export function selectionSort(chart) {
 
     for (let i = 0; i < n; i++) {
         let min = i;
-        setTimeout(function () {
-        for (let j = i + 1; j < n; j++) {
+setTimeout(function () {
+            for (let j = i + 1; j < n; j++) {
             if (chart.data[j] < chart.data[min]) {
                 min = j;
             }
@@ -15,7 +15,7 @@ export function selectionSort(chart) {
             chart.data[i] = chart.data[min];
             chart.data[min] = tmp;
             chart.update()
-        }
-        }, 10);
-    }
+        } 
+   }, 1); }
+
 }
